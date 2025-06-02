96d43523d8ec63bdbcc2086fb10789891ca9117e
Sango Mabhuti Yapi <yapisango@gmail.com>
2025-06-02 18:46:18 +0200

    Added icon
    Added chef claude png

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,7 @@
-//import chefClaudeLogo from "./images/chef-claude-icon.png"
-
 export default function Header() {
     return (
         <header>
-            {/* <img src={chefClaudeLogo}/> */}
+            <img src="/images/chef-claude-icon.png" alt="Chef Claude Logo"/>
             <h1>Chef Claude</h1>
         </header>
     )
